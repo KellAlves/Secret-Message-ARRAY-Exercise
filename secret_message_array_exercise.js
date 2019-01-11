ea8cc56c453e5fd855c9b291e0a8b2b9e3261afc
Kell Alves <41650515+KellAlves@users.noreply.github.com>
2019-01-11 12:19:36 -0200

    Create secret_message_array_exercise.js

---
+++ secret_message_array_exercise.js
@@ -0,0 +1,20 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+console.log(secretMessage.length); //logs in how many words
+
+secretMessage.pop(); //remove last word from
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'program'); //add words on
+console.log(secretMessage.length);
+
+secretMessage[7] = 'right'; //mutate item7
+
+secretMessage.shift(); //remove first word from
+
+secretMessage.unshift('Programming'); //adds word at the beginning
+
+const removeFrom = secretMessage.splice(6, 5, 'know, '); //removed the words "get, right, the, first, time," from position 6 FORWARD with lenght 5 and replaced by 'know, '
+
+console.log(secretMessage.join());
+
